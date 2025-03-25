1735d6239703fbabcd72faafec06c9be0d1f842b
JayRGarg <jayrgarg@gmail.com>
2025-03-25 04:20:32 -0700

    add consistent frame rate

--- main.py
+++ main.py
@@ -5,11 +5,20 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH,SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        dt = clock.tick(60) / 1000
+
         screen.fill("black")
         pygame.display.flip()
 
