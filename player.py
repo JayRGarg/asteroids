186113dde768fe55634c7fa064593d20f55183b6
JayRGarg <jayrgarg@gmail.com>
2025-03-25 16:37:34 -0700

    add move

--- player.py
+++ player.py
@@ -5,7 +5,7 @@ from constants import *
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
-        self.rotation = 0
+        self.rotation = 0 # degrees
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation) * self.radius
@@ -23,6 +23,12 @@ class Player(CircleShape):
         self.rotation += PLAYER_TURN_SPEED * dt
         return
 
+    def move(self, dt):
+        curr_direction = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += curr_direction * PLAYER_SPEED * dt
+        return
+        
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
@@ -30,3 +36,7 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
