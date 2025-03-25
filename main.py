6d146968184371181283916cedb9d6e7ecc7b746
JayRGarg <jayrgarg@gmail.com>
2025-03-25 16:27:03 -0700

    add rotation

--- main.py
+++ main.py
@@ -21,11 +21,14 @@ def main():
                 return
 
 
-        dt = clock.tick(60) / 1000
+        pl.update(dt)
 
         screen.fill("black")
         pl.draw(screen)
         pygame.display.flip()
+
+        # limit frame rate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
